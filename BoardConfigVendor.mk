6240eb557d24c95930a3a10ba4a832c7f1df3e77
Shirayuki39 <lorddemecrius83@proton.me>
2025-09-29 21:07:20 +0900

    LG8n: Sync with device tree
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,18 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    dpm \
+    dtbo \
+    gz \
+    lk \
+    logo \
+    mcupm \
+    md1img \
+    pi_img \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    tkv
